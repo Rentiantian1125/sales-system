faba6e2fab4c3eac9e5884a233c7c3c4d42d5ecd
rentiantian <271580437@qq.com>
2019-06-12 18:12:45 +0800

    2

--- sales/models.py
+++ sales/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 # 用户表
 class User(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
+    # id = models.IntegerField(primary_key=True)
     username = models.CharField(max_length=30)
     password = models.CharField(max_length=30, null=False)
     create_at = models.DateTimeField()
@@ -14,7 +15,9 @@ class User(models.Model):
 
 # 商品表
 class Goods(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30, null=False, default='')
+    # id = models.IntegerField(primary_key=True)
     price = models.IntegerField()
     num = models.IntegerField()
     create_at = models.DateTimeField()
@@ -25,15 +28,18 @@ class Goods(models.Model):
 
 # 采购订单表
 class PurchaseOrder(models.Model):
+    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete='CASCADE')
-    id = models.IntegerField(primary_key=True)
+    # id = models.IntegerField(primary_key=True)
     create_at = models.DateTimeField()
+    status = models.IntegerField()
 
 
 # 采购表
 class Purchase(models.Model):
+    id = models.AutoField(primary_key=True)
     order = models.ForeignKey(PurchaseOrder, on_delete='CASCADE')
-    id = models.IntegerField(primary_key=True)
+    # id = models.IntegerField(primary_key=True)
     goods_id = models.IntegerField()
     price = models.IntegerField()
     num = models.IntegerField()
@@ -45,15 +51,18 @@ class Purchase(models.Model):
 
 # 销售订单表
 class SellOrder(models.Model):
+    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete='CASCADE')
-    id = models.IntegerField(primary_key=True)
+    # id = models.IntegerField(primary_key=True)
     create_at = models.DateTimeField()
+    status = models.IntegerField()
 
 
 # 销售表
 class Sell(models.Model):
+    id = models.AutoField(primary_key=True)
     order = models.ForeignKey(SellOrder, on_delete='CASCADE')
-    id = models.IntegerField(primary_key=True)
+    # id = models.IntegerField(primary_key=True)
     goods_id = models.IntegerField()
     price = models.IntegerField()
     num = models.IntegerField()
@@ -65,8 +74,9 @@ class Sell(models.Model):
 
 # 库存表
 class Reserve(models.Model):
+    id = models.AutoField(primary_key=True)
     goods = models.ForeignKey(Goods, on_delete='CASCADE')
-    id = models.IntegerField(primary_key=True)
+    # id = models.IntegerField(primary_key=True)
     num = models.IntegerField()
     create_at = models.DateTimeField()
 
